091f0ded4737609d5b155c3c0e3b59a67b9dcfc9
Prabesh <prabesh.bista369@gmail.com>
2022-08-05 20:35:58 +0545

    [in-complete] personalized ,events,news,funds

---
+++ frontend_flutter/lib/views/funds/funds_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class FundsScreen extends StatelessWidget {
+  const FundsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+      return Scaffold(
+      appBar: AppBar(
+        title: const Text("Personalize Message"),
+      ),
+    );
+  }
+}
