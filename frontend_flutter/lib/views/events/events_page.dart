1707ffd0c6a3434018e391d8580eb0383ec97fca
Prabesh <prabesh.bista369@gmail.com>
2022-08-05 20:31:14 +0545

    [added] events page

---
+++ frontend_flutter/lib/views/events/events_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class EventsScreen extends StatelessWidget {
+  const EventsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+      appBar: AppBar(
+        title: const Text("Events"),
+      ),
+    );
+  }
+}
