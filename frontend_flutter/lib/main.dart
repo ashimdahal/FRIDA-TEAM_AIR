435334ea1309dbc2257adc01767610854a3fd067
Prabesh <prabesh.bista369@gmail.com>
2022-08-05 22:07:25 +0545

    [added] home page and refactored other screen

--- frontend_flutter/lib/main.dart
+++ frontend_flutter/lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:frontend_flutter/utils/theme.dart';
 import 'package:frontend_flutter/views/auth/login_page.dart';
+// import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
   runApp(const MyApp());
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
 }
 
 class MyApp extends StatelessWidget {
@@ -11,10 +17,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-        home:const  LoginPage(),
-        themeMode: ThemeMode.light,
-        theme: MyTheme.lightTheme(),
+    return MaterialApp(
+      home: const LoginPage(),
+      themeMode: ThemeMode.light,
+      theme: MyTheme.lightTheme(),
     );
   }
 }
